889e7f1219bb53f4cda4c98661339a74ba85ff4d
Alejandro Salgado <alejandrosram@outlook.com>
2022-08-13 01:24:40 -0500

    Update fundamentals with indecision and counter components

---
+++ src/components/Counter.vue
@@ -0,0 +1,69 @@
+<template>
+	<h2>{{ customTitle }}</h2>
+
+	<p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+
+	<div>
+		<button @click="increment">+1</button>
+		<button @click="decrement">-1</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Counter",
+	props: {
+		title: String,
+		start: {
+			type: Number,
+			// required: true,
+			default: 0,
+			validator(value) {
+				return value >= 0;
+			}
+		},
+	},
+	data() {
+		return {
+			counter: this.start,
+		};
+	},
+	methods: {
+		increment() {
+			this.counter++;
+		},
+		decrement() {
+			if (this.counter <= 0) {
+				return;
+			}
+			this.counter--;
+		},
+	},
+	computed: {
+		squareCounter() {
+			return this.counter * this.counter;
+		},
+		customTitle() {
+			return this.title || "Counter";
+		},
+	},
+};
+</script>
+
+<style>
+button {
+	background-color: #64BB87;
+	border-radius: 10px;
+	border: 1px solid white;
+	color: white;
+	cursor: pointer;
+	margin: 0 5px;
+	padding: 5px 15px;
+	transition: 0.3s ease-in-out;
+}
+
+button:hover {
+	background-color: #5AA67B;
+	transition: 0.3s ease-in-out;
+}
+</style>
